6d1991b9040a35527a22cd7f8a89f26304b10076
Assaf Gordon <assafgordon@gmail.com>
2016-06-21 02:59:52 -0400

    openbsd-5.9: adjust openbsd-dummy header for Debian
    Add missing <bsd.h> include,
    comment-out reallocarray (provided by -lbsd).

--- openbsd-5.9/openbsd-dummy.h
+++ openbsd-5.9/openbsd-dummy.h
@@ -1,6 +1,7 @@
 #ifndef __OPENBSD_DUMMY__
 
 #include <stdlib.h>
+#include <bsd/bsd.h>
 
 #define srandom_deterministic srandom
 
@@ -12,11 +13,13 @@ pledge(const char * promises, const char * paths [])
   return 0;
 }
 
+/*
 static inline void*
 reallocarray(void *ptr, size_t nmemb, size_t size)
 {
-  /* unsafe, without checking for integer overflow */
+  // unsafe, without checking for integer overflow
   return realloc(ptr, size*nmemb);
 }
+*/
 
 #endif
